da3f882e25f71a936dd7b85ef9b3a24df7625629
Sam Contapay <sam.contapay@gmail.com>
2012-06-28 11:20:41 -0700

    Initial Commit

---
+++ Cam/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Cam
+//
+//  Created by Sam Contapay on 6/28/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
